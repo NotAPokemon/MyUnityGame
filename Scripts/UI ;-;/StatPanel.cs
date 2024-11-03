31c007b2b8586f7e95dce26f59a4e6282c3c4e10
Arvan Kadkol <92272566+NotAPokemon@users.noreply.github.com>
2024-11-02 18:17:45 -0700

    fixed stats display error caused by changing the color

--- Scripts/UI ;-;/StatPanel.cs
+++ Scripts/UI ;-;/StatPanel.cs
@@ -4,25 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class StatPanel : MonoBehaviour
 {
     public List<Stat> stats;
     public GameObject statPrefab;
+    bool made = false;
 
-    private void Start()
+    private void make()
     {
+        made = true;
         for (int i = 0; i < stats.Count; i++)
         {
             GameObject newStat = Instantiate(statPrefab, transform);
             newStat.SetActive(true);
-            newStat.transform.GetChild(0).GetComponent<Image>().sprite = stats[i].icon;
-            TextMeshProUGUI name = newStat.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-            TextMeshProUGUI amount = newStat.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
-            name.SetText(stats[i].displayName);
-            name.color = stats[i].color;
-            amount.SetText(stats[i].amount.ToString());
-            amount.color = stats[i].color;
             newStat.transform.localPosition = Vector3.zero + Vector3.down * 10 * i + Vector3.up * 44.7f;
 
         }
@@ -30,15 +26,20 @@ public class StatPanel : MonoBehaviour
 
     void updateStats()
     {
+        if (!made)
+        {
+            make();
+        }
         for (int i = 0; i < stats.Count; i++)
         {
             Transform temp = transform.GetChild(i);
+            temp.GetChild(0).GetComponent<Image>().sprite = stats[i].icon;
             TextMeshProUGUI name = temp.GetChild(1).GetComponent<TextMeshProUGUI>();
             TextMeshProUGUI amount = temp.GetChild(2).GetComponent<TextMeshProUGUI>();
+            name.color = new Color(stats[i].color.r, stats[i].color.g, stats[i].color.b);
+            amount.color = new Color(stats[i].color.r, stats[i].color.g, stats[i].color.b);
             name.SetText(stats[i].displayName);
             amount.SetText(stats[i].amount.ToString());
-            name.ForceMeshUpdate();
-            amount.ForceMeshUpdate();
         }
     }
 
